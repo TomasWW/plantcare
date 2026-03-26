535c122672baa82851f858192056191298bbab37
TomasWW <tomas.woelflin@gmail.com>
2026-03-26 09:37:34 +0000

    feat: add email login and unique constraint to user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
     class Meta:
-        db_table = 'user'
+        db_table = 'users'
